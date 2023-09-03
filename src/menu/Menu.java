bee2d60383118b5f904a593233f353acc64dd950
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-09-03 18:12:49 -0300

    Resolución de Patricio

---
+++ src/menu/Menu.java
@@ -0,0 +1,98 @@
+package menu;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Menu extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	private JButton ejercicio_1, ejercicio_2, ejercicio_3;
+	private NuestroLabel lblGrupo;
+	
+	public Menu() {
+		initComponents();
+		initUi();
+		initListeners();
+		setVisible(true);
+	}
+	
+	private void initListeners() {
+		ejercicio_1.addActionListener(new ListenerMenu(ejercicio_1));
+		ejercicio_2.addActionListener(new ListenerMenu(ejercicio_2));
+		ejercicio_3.addActionListener(new ListenerMenu(ejercicio_3));
+		
+	}
+
+	private void initUi() {
+		
+		setBounds(400,400, 600, 400);
+		setTitle("");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		getContentPane().setLayout(null);
+		ejercicio_1.setBounds(220, 100, 100, 30);
+		ejercicio_2.setBounds(220, 150, 100, 30);
+		ejercicio_3.setBounds(220, 200, 100, 30);
+		lblGrupo.setBounds(40, 10, 200, 50);
+		getContentPane().add(ejercicio_1);
+		getContentPane().add(ejercicio_2);
+		getContentPane().add(ejercicio_3);
+		getContentPane().add(lblGrupo);
+	}
+
+	public void initComponents() {
+		lblGrupo = new NuestroLabel("GRUPO NRO: 4");
+		ejercicio_1 = new JButton("Ejercicio 1");
+		ejercicio_2 = new JButton("Ejercicio 2");
+		ejercicio_3 = new JButton("Ejercicio 3");
+	}
+		
+	
+	
+///es de prueba
+	public static void main(String[] args) {
+	Menu menu= new Menu();
+	
+	}
+	
+}
+
+class ListenerMenu implements ActionListener {
+	
+	JButton boton;
+	
+	public ListenerMenu() {
+		boton= null;
+	}
+	public ListenerMenu(JButton btn) {
+		boton= btn;
+	}
+	
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+
+	switch(boton.getText()) {
+	case "Ejercicio 1": 
+		///comentar los system.out cuando hacemos las ventanas
+		System.out.println("1");
+		//Abrir ventana
+		break;
+	case "Ejercicio 2": 
+		System.out.println("2");
+		break;
+	case "Ejercicio 3": 
+		System.out.println("3");
+		
+		break;
+		
+	default:
+		break;
+	}
+	
+}
+
+}
